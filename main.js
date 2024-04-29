c6f695de1a76c12a43484138135c02d8dfc90d22
aleyrbag <164878699+aleyrbag@users.noreply.github.com>
2024-04-29 10:00:21 -0300

    p

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-15T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-00-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
